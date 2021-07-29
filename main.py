0f5341f34ec7a38e8e8755ac06aa7d3fcfa47462
Sakib714 <sakib39.official@gmail.com>
2021-07-30 01:29:37 +0600

    modified main file

--- main.py
+++ main.py
@@ -10,4 +10,5 @@ url = 'https://github.com/'+github_user
 r = requests.get(url)
 soup = bs(r.content, 'html.parser')
 profile_image = soup.find('img', {'alt': 'Avatar'})['src']
+
 print(profile_image)
